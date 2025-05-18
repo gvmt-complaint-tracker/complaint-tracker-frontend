6de985060956b90c2c68e8f24f2f78307b57a73f
IsiteYves <yvesisite@gmail.com>
2025-05-18 08:56:09 -0300

    feat-properly show live environment

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("we outside", import.meta.env.VITE_API_BASE_URL);
 const API = axios.create({
   baseURL: import.meta.env.API_BASE_URL || "http://localhost:8000/api",
 });
